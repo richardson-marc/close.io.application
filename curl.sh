51ff06f15a6a7c9bb5bd227b1dae61c640bbc5d8
richardson-marc <marc.richardson@leankit.com>
2018-01-13 13:22:48 -0500

    small change

--- curl.sh
+++ curl.sh
@@ -1,2 +1,2 @@
-curl -i -X POST -H "Content-Type: application/json" -d '{"first_name":"Marc" ,"last_name":"Richardson","email":"marcr@blarg.net","phone":"2063834398","urls":["https://github.com/richardson-marc","https://linkedin.com/in/marcrichardson1"]}' https://app.close.io/hackwithus/
+curl -i -X POST -H "Content-Type: application/json" -d '{"first_name":"Marc" ,"last_name":"Richardson","email":"marcr@blarg.net","phone":"2063834398","cover_letter":"hello","urls":["https://github.com/richardson-marc","https://linkedin.com/in/marcrichardson1"]}' https://app.close.io/hackwithus/
 
--- sort.sh
+++ sort.sh
@@ -1,1 +1,2 @@
-grep closeio.html log.txt |egrep -v "(127.0.0.1|::1)"| cut -f 2 -d " "|sort | uniq -c
+grep closeio.html log.txt |egrep -v "(127.0.0.1|::1)"| cut -f 2 -d " "|sort | uniq -c| sort -n
+
